549181d9300b31c152909100fddaed5ccd0bef1b
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2017-04-21 17:33:18 +0200

    make codacy happy

--- tests/test_error_vis.py
+++ tests/test_error_vis.py
@@ -38,10 +38,7 @@ class TestErrorVis:
         settings = manager.settings_for_view(self.view)
         settings.use_libclang = self.use_libclang
 
-        if error_style == PHANTOMS:
-            settings.show_phantoms_for_errors = True
-        else:
-            settings.show_phantoms_for_errors = False
+        settings.show_phantoms_for_errors = bool(error_style == PHANTOMS)
 
         view_config_manager = ViewConfigManager()
         view_config = view_config_manager.load_for_view(self.view, settings)
