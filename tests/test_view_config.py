34b67b3593c96a9cdb618132802a8996e5b5656a
Igor Bogoslavskyi <igor.bogoslavskyi@gmail.com>
2018-01-28 15:37:51 +0100

    Remove print statement

--- tests/test_view_config.py
+++ tests/test_view_config.py
@@ -72,7 +72,6 @@ class TestViewConfig(GuiTestWrapper):
         completer = view_config.completer
         self.assertEqual(len(completer.clang_flags), 14)
         # test from the start
-        print(completer.clang_flags)
         self.assertEqual(completer.clang_flags[0], '-c')
         self.assertEqual(completer.clang_flags[1], '-fsyntax-only')
         self.assertEqual(completer.clang_flags[2], '-x')
