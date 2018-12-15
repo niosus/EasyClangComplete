3b6ff055094fb7b89b7fbaccb3092e74bc1d4fee
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2018-12-15 21:08:52 +0100

    Add references to popup + refactoring

---
+++ plugin/utils/index_location.py
@@ -0,0 +1,22 @@
+"""Class that mimics cursor.location for dealing with index locations."""
+
+from os import path
+
+
+class IndexLocation:
+    """Location of a file that mimics Cursor.location."""
+
+    class File:
+        """Help class to mimic Cursor.file."""
+
+        def __init__(self, name):
+            """Initialize file name."""
+            self.name = name
+            self.extension = path.splitext(name)[1]
+            self.short_name = path.basename(name)
+
+    def __init__(self, filename, line, column):
+        """Initialize new location."""
+        self.file = IndexLocation.File(filename)
+        self.line = line
+        self.column = column
