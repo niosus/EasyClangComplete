76898f49bc7c6ca1d94405ac800103f6a79a1569
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2018-12-23 19:11:04 +0100

    Update tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,10 +1,16 @@
 """Tests for settings."""
 import sublime
+import imp
 
 from os import path
 
-from EasyClangComplete.plugin.settings.settings_manager import SettingsManager
 from EasyClangComplete.tests.gui_test_wrapper import GuiTestWrapper
+
+from EasyClangComplete.plugin.settings import settings_manager
+
+imp.reload(settings_manager)
+
+SettingsManager = settings_manager.SettingsManager
 
 
 class test_settings(GuiTestWrapper):
@@ -64,10 +70,6 @@ class test_settings(GuiTestWrapper):
         settings = manager.settings_for_view(self.view)
         dirs = settings.common_flags
 
-        current_folder = path.dirname(self.view.file_name())
-        parent_folder = path.dirname(current_folder)
         self.assertTrue(len(initial_common_flags) <= len(dirs))
         self.assertTrue(initial_common_flags[0] in dirs)
         self.assertFalse(initial_common_flags[1] in dirs)
-        self.assertTrue(("-I" + current_folder) in dirs)
-        self.assertTrue(("-I" + parent_folder) in dirs)
