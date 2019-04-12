d05ccc781f3ad859bfdc9f2312622f12ffc72d81
igor.bogoslavskyi@uni-bonn.de <igor.bogoslavskyi@uni-bonn.de>
2019-04-12 22:39:03 +0200

    Add tests with include path adaptation

--- tests/test_include_parser.py
+++ tests/test_include_parser.py
@@ -12,7 +12,7 @@ class TestIncludeParser(TestCase):
     """Test unique list."""
 
     def test_get_all_includes(self):
-        """Test initialization."""
+        """Test getting all includes."""
         base_folder = path.dirname(__file__)
         _, res = include_parser.get_all_headers(
             folders=[base_folder],
@@ -31,7 +31,7 @@ class TestIncludeParser(TestCase):
         self.assertIn(expected_completion, res)
 
     def test_get_specific_includes(self):
-        """Test initialization."""
+        """Test getting only specific includes."""
         base_folder = path.dirname(__file__)
         _, res = include_parser.get_all_headers(
             folders=[base_folder],
@@ -40,6 +40,20 @@ class TestIncludeParser(TestCase):
             completion_request=None)
         self.assertEqual(len(res), 1)
         local_file_path = 'cmake_tests/lib/a.h'
+        expected_completion = [
+            '{}\t{}'.format(local_file_path, base_folder), local_file_path]
+        self.assertIn(expected_completion, res)
+
+    def test_get_includes_with_adaptation(self):
+        """Test getting includes adapting paths to platform."""
+        base_folder = path.dirname(__file__)
+        _, res = include_parser.get_all_headers(
+            folders=[base_folder],
+            prefix='cmake_',
+            platform_specific_includes=True,
+            completion_request=None)
+        self.assertEqual(len(res), 1)
+        local_file_path = path.normpath('cmake_tests/lib/a.h')
         expected_completion = [
             '{}\t{}'.format(local_file_path, base_folder), local_file_path]
         self.assertIn(expected_completion, res)
